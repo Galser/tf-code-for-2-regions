23afaa238a6e3b56a8a6d9dac89f73dd9fed043e
Andrii Guselietov <guseletov@gmail.com>
2020-12-17 18:12:35 +0100

    Main code ready

--- variables.tf
+++ variables.tf
@@ -1,13 +1,24 @@
 variable "region1" {
-	default = "eu-central-1"
+	default = "eu-west-1"
 }
 
-
 variable "region2" {
-	default = "eu-west-3"
+	default = "eu-west-2"
 }
 
 
 variable "ubuntu_account_number" {
   default = "099720109477"
+}
+
+variable "testname" {
+  default = "agtest"
+}
+
+variable "tag" {
+  default = "guselietov_test_2"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
 }
